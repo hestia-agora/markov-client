4dc9d7bc3a8fa7e467e8cd3030dacb02da3af4b9
Mihaela Maxim <maxim.mihaela1996@gmail.com>
2025-01-18 15:09:30 +0100

    Define .env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://web-production-744e.up.railway.app';
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ||
+  (process.env.NODE_ENV === 'development'
+    ? 'http://127.0.0.1:5000'
+    : 'https://web-production-744e.up.railway.app');
 
 export const runModel = async (params) => {
-    try {
-        const response = await axios.post(
-            `${API_BASE_URL}/`,
-            params,
-            { headers: { 'Content-Type': 'application/json' } }
-        );
-        return response.data;
-    } catch (error) {
-        console.error("Error running model:", error.message || error);
-        throw error;
-    }
+  try {
+    const response = await axios.post(
+      `${API_BASE_URL}/`,
+      params,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error running model:", error.message || error);
+    throw error;
+  }
 };
